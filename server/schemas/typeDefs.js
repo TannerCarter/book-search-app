367f4e3e79964e74051866008bab8bd899391cbb
TannerCarter <tannerc.bootcamp@gmail.com>
2021-09-29 16:38:18 -0700

    Added typeDefs, updated server js to include a log to show where graphql is

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,48 @@
-//typeDefs Here, base added.
-
-// import the gql tagged template function
 const { gql } = require("apollo-server-express");
 
-// create our typeDefs
-const typeDefs = gql``;
+//Query, Mutation, User, Book, Auth
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
 
-// export the typeDefs
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String]
+    bookId: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+`;
+
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -9,14 +9,26 @@ const { typeDefs, resolvers } = require("./schemas");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Implement Apollo Server -
-const server = new ApolloServer({
-  typeDefs, //Needs require
-  resolvers, //Needs require
-  context: authMiddleware,
-});
-//Apply to the Expresss server as middleware
-server.applyMiddleware({ app });
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
